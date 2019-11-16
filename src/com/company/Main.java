308cb11e1d041c86f0ac91fa0c06b46e31b98820
victortwin <42267540+victortwin@users.noreply.github.com>
2019-11-16 16:54:03 +0400

    edit line "Hello Java"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Java");
+        System.out.println("Hello");
 	System.out.println("This line is added from Github");    
     }
 }
