678cc1d99607bf1f0569849110da2b8ac5eecae7
victortwin <42267540+victortwin@users.noreply.github.com>
2019-11-16 16:40:27 +0400

    add new line more

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Java");
+	System.out.println("This line is added from Github");    
     }
 }
